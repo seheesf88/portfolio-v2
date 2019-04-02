8d72ff3959d9cf52cf9e1b51f03914417ee49744
seheesf88 <seheesf88@gmail.com>
2019-04-02 01:42:29 -0700

    adding contact

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { SocialIcon } from 'react-social-icons';
+import { Collapse, Button, ButtonToolbar, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 class Contact extends Component{
   constructor(){
@@ -8,8 +10,77 @@ class Contact extends Component{
 
   render(){
     return(
-      <div>
+      <div className="contact">
+        <h2>Conatct</h2>
+        <div className="contract grid-container">
+          <div className="contract grid-item">
+            <div className="namecard">
+              <div>SeHee Son</div>
+              <div>San Francisco, CA</div>
+              <div>seheesf88@gmail.com</div>
+            </div>
 
+            <div>
+              <span>
+                <a href="mailto:seheesf88@gmail.com" data-tooltip="hi?">
+                <img src="./contact/envelope.png" id="emailIcon"/>
+                </a>
+                <span className="mailtext"><a href="mailto:seheesf88@gmail.com" data-tooltip="hi?" id="mailtext">Email me</a></span>
+              </span>
+
+              <span className="divider" id="divider">|</span>
+
+              <span className="github">
+                <OverlayTrigger
+                  key='bottom'
+                  placement='bottom'
+                  overlay={
+                    <Tooltip className="tooltip">
+                      see my code?
+                    </Tooltip>
+                  }
+                >
+                  <SocialIcon network="github" style={{ height: 40, width: 40 }} bgColor="#DFD9D9" url="https://git.generalassemb.ly/seheesf88" />
+                </OverlayTrigger>
+                <a href="https://git.generalassemb.ly/seheesf88" className="githubtext">Github</a>
+              </span>
+
+              <span className="divider" id="divider">|</span>
+
+              <span className="linkedin">
+                <OverlayTrigger
+                  key='bottom'
+                  placement='bottom'
+                  overlay={
+                    <Tooltip className="tooltip">
+                      know more about me?
+                    </Tooltip>
+                  }
+                >
+                  <SocialIcon style={{ height: 40, width: 40 }} bgColor="#DFD9D9" url="http://linkedin.com/in/sehee-son/" />
+                </OverlayTrigger>
+                <a href="https://git.generalassemb.ly/seheesf88/" className="linkedintext">linkedin</a>
+              </span>
+
+              <span className="divider" id="divider">|</span>
+
+              <span>
+              <OverlayTrigger
+                key='bottom'
+                placement='bottom'
+                overlay={
+                  <Tooltip className="tooltip">
+                    Download my resume
+                  </Tooltip>
+                }
+              >
+                <a href="../Resume_SeHee_Son.pdf" download><img src="./contact/download.png" style={{ height: 36, width: 38 }} id="download"/></a>
+              </OverlayTrigger>
+              <a href="/" download className="download">Download Resume</a>
+              </span>
+              </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 class Projects extends Component{
   constructor(){
@@ -13,14 +14,19 @@ class Projects extends Component{
         <div className="card grid-container">
 
           <div className="card grid-item">
-            <img src="./projects/seestar.png" className="pic"/>
+            <img src="./projects/mytripplanner.png" className="pic"/>
             <h4>My Trip Planner</h4>
             <div className="description">
               This application is what a user can use to plan and manage a trip budget and itinerary. The user can share this information with other users. This app includes a calendar and budget calculator.
             </div>
-            <div className="link">
-              <div><a href="">My Trip Planner</a></div>
-              <div><a href="">Github</a></div>
+            <div className="links ">
+              <div className="link"><img src="./projects/heroku.png" className="heroku" /><a href="https://react-tripplanner.herokuapp.com" className="aheroku">Flight Tracker</a></div>
+              <div>
+                <span><SocialIcon network="github" style={{ height: 25, width: 25 }} bgColor="#203960" /></span>
+                <span><a href="https://git.generalassemb.ly/seheesf88/react-project5" className="frontend">Front-end</a></span>
+                <span className="divider">|</span>
+                <span><a href="https://git.generalassemb.ly/seheesf88/project5-back-end" className="backend">Backend</a></span>
+              </div>
             </div>
             <div className="pro badges">
               <span className="pro badge">JavaScript</span>
@@ -30,6 +36,7 @@ class Projects extends Component{
               <span className="pro badge">Mongoose</span>
               <span className="pro badge">Session</span>
               <span className="pro badge">RESTful API</span>
+              <span className="pro badge">CRUD pattern</span>
               <span className="pro badge">Bootstraps</span>
               <span className="pro badge">CSS</span>
               <span className="pro badge">Semantic UI</span>
@@ -40,9 +47,14 @@ class Projects extends Component{
             <img src="./projects/seestar.png" className="pic"/>
             <h4>SeeStar</h4>
             <div className="description">This application is a clone Instagram app. The user can post and share their pictures and stories and view other user's pictures and story. Users also can add comments to other user's posting.</div>
-            <div className="link">
-              <div><a href="">SeeStar</a></div>
-              <div><a href="">Github</a></div>
+            <div className="links">
+              <div className="link"><img src="./projects/heroku.png" className="heroku" /><a href="https://star-frontend.herokuapp.com" className="aheroku">Flight Tracker</a></div>
+              <div>
+                <span><SocialIcon network="github" style={{ height: 25, width: 25 }} bgColor="#203960" /></span>
+                <span><a href="https://git.generalassemb.ly/seheesf88/project4" className="frontend">Front-end</a></span>
+                <span className="divider">|</span>
+                <span><a href="https://git.generalassemb.ly/seheesf88/project4-backend" className="backend">Backend</a></span>
+              </div>
             </div>
             <div className="pro badges">
               <span className="pro badge">JavaScript</span>
@@ -50,6 +62,7 @@ class Projects extends Component{
               <span className="pro badge">React</span>
               <span className="pro badge">Flask</span>
               <span className="pro badge">PostgreSQL</span>
+              <span className="pro badge">CRUD pattern</span>
               <span className="pro badge">Bootstraps</span>
               <span className="pro badge">CSS</span>
               <span className="pro badge">Semantic UI</span>
@@ -57,14 +70,19 @@ class Projects extends Component{
             </div>
           </div>
           <div className="card grid-item">
-            <img src="./projects/seestar.png" className="pic"/>
-            <h4>SeeStar</h4>
+            <img src="./projects/flighttracker.png" className="pic"/>
+            <h4>Flight Tracker</h4>
             <div className="description">
               This application is for a user to track flights and share reports about delays. The user can search flights by entering flight numbers and see flight information and reports about delays from other users.
             </div>
-            <div className="link">
-              <div><a href="">SeeStar</a></div>
-              <div><a href="">Github</a></div>
+            <div className="links">
+              <div className="link"><img src="./projects/heroku.png" className="heroku" /><a href="https://flight-react.herokuapp.com/" className="aheroku">Flight Tracker</a></div>
+              <div>
+                <span><SocialIcon network="github" style={{ height: 25, width: 25 }} bgColor="#203960" /></span>
+                <span><a href="https://git.generalassemb.ly/seheesf88/project3-flight-react" className="frontend">Front-end</a></span>
+                <span className="divider">|</span>
+                <span><a href="https://git.generalassemb.ly/seheesf88/project3-flight-api" className="backend">Backend</a></span>
+              </div>
             </div>
             <div className="pro badges">
               <span className="pro badge">JavaScript</span>
@@ -73,6 +91,7 @@ class Projects extends Component{
               <span className="pro badge">RESTful API</span>
               <span className="pro badge">MongoDB</span>
               <span className="pro badge">Moongose</span>
+              <span className="pro badge">CRUD pattern</span>
               <span className="pro badge">Bootstraps</span>
               <span className="pro badge">CSS</span>
               <span className="pro badge">JSX</span>
