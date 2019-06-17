6b8c32f839e4473c5b68595bc94a84c82c2283cd
seheesf88 <seheesf88@gmail.com>
2019-06-16 18:50:15 -0700

    edit

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -14,7 +14,7 @@ class Profile extends Component{
         <Nav />
         <div id="propic"><img src="./profile/profile-v2.png" style={{height: 120, width:120}}/></div>
         <div className="intro">
-          <p>Hi, nice to meet you! I am highly self-motivated junior full stack developer that loves to learn code.</p>
+          <p>Hi, nice to meet you! I am highly self-motivated junior full stack web developer who loves to learn code.</p>
           <p>I have worked in the hospitality industry and medical fields for 10 years and have gained various experiences and skills in how to work in part of team and in a fast-paced work environment.</p>
           <p>I strongly believe that I can perform amazing work by dedicating my professional technics and experiences.</p>
         </div>
